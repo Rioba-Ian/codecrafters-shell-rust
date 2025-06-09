9d94ac436c4c65a0adecef904fa6fe49f3c0f374
Rioba-Ian <riobafelix@gmail.com>
2025-06-09 14:37:22 +0300

    repl and continue getting user input

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,23 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
 
     // Wait for user input
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
 
-    println!("{}: command not found", input.trim())
+    loop {
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let trimmed_input = input.trim();
+
+        if trimmed_input == "exit" {
+            println!("Exiting program.");
+            break;
+        }
+
+        println!("{}: command not found", trimmed_input);
+    }
 }
