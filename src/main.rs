eb5fdfaa406fb7b9c25b60a42979888dbb90737a
Rioba-Ian <riobafelix@gmail.com>
2025-06-10 14:55:03 +0300

    parse inputs and parse echo command

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,21 @@ fn main() {
         match trimmed_input {
             "exit 0" => break,
             "quit" => break,
-            _ => println!("{}: command not found", trimmed_input),
+            _ => parse_input(trimmed_input),
         }
 
         input.clear();
+    }
+}
+
+fn parse_input(input: &str) {
+    let input_str = input.to_string();
+
+    if input_str.starts_with("echo") {
+        let echo_data: Vec<String> = input.split(" ").map(|s| s.to_string()).collect();
+        let parsed_data = &echo_data[1..].join(" ");
+        println!("{}", parsed_data);
+    } else {
+        println!("{}: command not found", input)
     }
 }
